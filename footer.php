2aa161d933b3e4b6aeaed6ee6f32c06c00e6f244
Juan Felipe Gomez <jf.gomezramirez@gmail.com>
2022-09-29 08:07:18 -0700

    widget created

--- footer.php
+++ footer.php
@@ -1,4 +1,11 @@
 
+<footer>
+  <div class="footer-container">
+    <?php dynamic_sidebar('footer') ?>
+    <!-- The argument of dynamic_sidebar should be the id of the sideber I created -->
+  </div>
+</footer>
+
 <?php wp_footer(); ?> 
 
 </body>
